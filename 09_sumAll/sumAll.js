94ad23e553b9e9384cfa51bad1c200f9bcda481f
jnbn98 <jan.benn@mni.thm.de>
2025-05-10 14:08:40 +0200

    fixed 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  // Check if inputs are numbers
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "ERROR";
+  }
 
+  // Check if inputs are negative
+  if (numOne < 0 || numTwo < 0) {
+    return "ERROR";
+  }
+
+  // Check if inputs are integers
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+    return "ERROR";
+  }
+
+  // Swap if needed
+  if (numTwo < numOne) {
+    let temp = numOne;
+    numOne = numTwo;
+    numTwo = temp;
+  }
+
+  let erg = 0;
+  for (let i = numOne; i <= numTwo; i++) {
+    erg += i;
+  }
+  return erg;
 };
-
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
