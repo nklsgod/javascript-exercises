b4fa1cfc7291c2255efe5afc553693f14c27ff2a
jnbn98 <jan.benn@mni.thm.de>
2025-05-10 13:54:57 +0200

    adding arrays

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeItems) {
+  for (let i = 0; i < array.length; i++) {
+    if (removeItems.includes(array[i])) {
+      array.splice(i, 1);
+      i--; // Decrement i to adjust for the removed element
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
