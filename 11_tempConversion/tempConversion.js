3ee0901677b0f5f765e3a1f4f1ae3d287dd888ea
jnbn98 <jan.benn@mni.thm.de>
2025-05-10 14:23:05 +0200

    fixed 10

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = 0;
+
+  celsius = ((temp - 32) * 5) / 9;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = 0;
+  fahrenheit = temp * 1.8 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
